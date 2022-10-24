6b4860c6e117b43a477ae9f06f02beae1c6ad545
gala377 <r.galczak@gmail.com>
2022-10-24 22:05:09 +0200

    Use bump allocator in the example to get rid of the unused warnings.

--- classy_example/src/main.rs
+++ classy_example/src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use classy_vm::mem::allocator::Allocator;
+use classy_vm::mem::{allocator::Allocator, bump::BumpAllocator, page::Page};
 
 const PAGE_SIZE: usize = 1 << 12;
 const PAGE_ALIGN: usize = 1 << 12;
@@ -15,12 +15,20 @@ struct Args {
 
     #[arg(long, default_value_t = PAGE_ALIGN)]
     page_align: usize,
+
+    #[arg(long, default_value_t = 0)]
+    allocate_integers: usize,
 }
 
 fn main() {
+    println!("Size and align of page: {} / {}", std::mem::size_of::<Page>(), std::mem::align_of::<Page>());
     let args = Args::parse();
     let mut alloc = Allocator::new();
-    for _ in 0..args.pages_count {
+    if args.pages_count == 0{
+        return;
+    }
+    let page = alloc.allocate_page(args.page_size, args.page_align);
+    for _ in 1..args.pages_count {
         alloc.allocate_page(args.page_size, args.page_align);
     }
     println!(
@@ -28,4 +36,13 @@ fn main() {
         n = args.pages_count,
         sum = args.pages_count * args.page_size
     );
+
+    println!("Allocating integers");
+    let page = page.inner().expect("should not be none");
+    let mut bump = BumpAllocator::new(page);
+    for i in 0..args.allocate_integers {
+        if bump.alloc::<u64>().is_null() {
+            println!("Could not allocate integer {i}");
+        }
+    }
 }
