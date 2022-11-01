ed72ef59dff5db196e83012f2553c068207a6403
gala377 <r.galczak@gmail.com>
2022-11-01 19:40:40 +0100

    Added nonnull ptr and type erased ptrs

--- classy_vm/src/mem/ptr.rs
+++ classy_vm/src/mem/ptr.rs
@@ -48,3 +48,40 @@ impl<T> Clone for Ptr<T> {
 }
 
 impl<T> Copy for Ptr<T> {}
+
+pub struct NonNullPtr<T>(pub NonNull<T>);
+
+impl<T> NonNullPtr<T> {
+
+    #[inline(always)]
+    pub fn new(ptr: NonNull<T>) -> Self {
+        Self(ptr)
+    } 
+
+    pub fn inner(&self) -> NonNull<T> {
+        self.0
+    }
+
+    pub unsafe fn cast<U>(&self) -> NonNullPtr<U> {
+        let ptr = self.0;
+        assert!(
+            (ptr.as_ptr() as usize) % std::mem::align_of::<U>() == 0,
+            "cannot cast pointer to type {} because the aligment is mismatched",
+            std::any::type_name::<U>()
+        );
+        NonNullPtr(ptr.cast())
+    }
+}
+
+impl<T> Clone for Ptr<T> {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
+
+impl<T> Copy for Ptr<T> {}
+
+
+pub type ErasedPtr = Ptr<()>;
+
+pub type ErasedNonNull = NonNullPtr<()>;
