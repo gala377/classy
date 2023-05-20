7a7d229dcbd0eca42880f107190f6f78ff95a92a
gala377 <r.galczak@gmail.com>
2023-05-20 22:16:19 +0200

    Add empty moduels for future passes.

--- classy_c/src/ast_passes/mod.rs
+++ classy_c/src/ast_passes/mod.rs
@@ -1,6 +1,8 @@
 use crate::syntax::ast;
 
 pub mod func_to_struct_literal;
+pub mod verify_lvalues;
+pub mod promote_local_types;
 
 pub trait AstPass {
     fn run(&mut self, ast: ast::Program) -> ast::Program;
