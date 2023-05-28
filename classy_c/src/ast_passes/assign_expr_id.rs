148a68c4f448d3b32275d55298205466192c2d5d
gala377 <r.galczak@gmail.com>
2023-05-29 00:01:52 +0200

    Add pass that assigns ids to ast nodes

---
+++ classy_c/src/ast_passes/assign_expr_id.rs
@@ -0,0 +1,30 @@
+use crate::syntax::ast::{Folder, self};
+
+use super::AstPass;
+
+
+
+pub struct AssignExprId {
+    id: usize,
+}
+
+impl AssignExprId {
+    pub fn new() -> Self {
+        Self { id: 1 }
+    }
+}
+
+impl AstPass for AssignExprId {
+    fn run(&mut self, ast: ast::Program) -> ast::Program {
+        self.fold_program(ast)
+    }
+}
+
+impl Folder for AssignExprId {
+    fn fold_expr(&mut self, expr: ast::Expr) -> ast::Expr {
+        let mut expr = ast::fold::fold_expr(self, expr);
+        expr.id = self.id;
+        self.id += 1;
+        expr
+    }
+}
--- classy_c/src/ast_passes/mod.rs
+++ classy_c/src/ast_passes/mod.rs
@@ -3,6 +3,7 @@ use crate::{syntax::ast, typecheck::type_context::TypCtx};
 pub mod func_to_struct_literal;
 pub mod promote_local_types;
 pub mod verify_lvalues;
+pub mod assign_expr_id;
 
 pub trait AstPass {
     fn run(&mut self, ast: ast::Program) -> ast::Program;
@@ -11,6 +12,7 @@ pub trait AstPass {
 pub fn run_befor_type_context_passes(ast: ast::Program) -> ast::Program {
     let ast = verify_lvalues::VerifyLvalues.run(ast);
     let ast = promote_local_types::PromoteAnonTypes::new().run(ast);
+    let ast = assign_expr_id::AssignExprId::new().run(ast);
     ast
 }
 
