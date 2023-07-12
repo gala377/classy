bc0855167bf3b4d185952518a513ecf696813e3d
gala377 <r.galczak@gmail.com>
2023-07-12 17:58:39 +0200

    documentation for the tuple class as it is not trivial.

--- classy_vm/src/runtime/class/tuple.rs
+++ classy_vm/src/runtime/class/tuple.rs
@@ -7,6 +7,17 @@ use super::{header::Header, Class, Kind};
 const SIZE_MASK: usize = 0b111111;
 const TAG_SHIFT: usize = 6;
 
+/// Tuple uses its data field to store its length
+/// (on the first 6 bytes). The rest of the bytes
+/// are used as a bitmap to indicate which fields
+/// are references.
+/// 
+/// So teh format of the data looks like so
+/// 
+/// [bitmap: 64-6 bytes][length: 6 bytes]
+/// 
+/// the most rightmost bit of the bitmap corresponds
+/// to the first field of the tuple and so on.
 pub const TUPLE_CLASS: Class = Class {
     name: Ptr::null(),
     instance_size: size_of::<usize>(),
