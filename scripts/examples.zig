53f375f750e245d280083453dba29a53b3ff57de
gala377 <r.galczak@gmail.com>
2023-08-23 22:26:17 +0200

    Refactor a zig script so it looks a bit nicer

--- scripts/examples.zig
+++ scripts/examples.zig
@@ -11,30 +11,57 @@ pub fn main() !void {
     const allocator = arena.allocator();
 
     // parse args
-    const args = try std.process.argsAlloc(allocator);
-    if (args.len < 2) {
-        std.debug.print("usage: {s} <file> <forward-args>", .{std.fs.path.basename(args[0])});
-        return;
-    }
+    const args = try parse_args(allocator);
     // get file path
-    var buf: [std.fs.MAX_PATH_BYTES]u8 = undefined;
-    const cwd = try std.os.getcwd(&buf);
-    const file_path = try std.fs.path.join(allocator, &[_][]u8{ cwd, args[1] });
-    std.debug.print("got path: {s}", .{file_path});
+    const file_path = try expand_file_path(allocator, args[1]);
 
     // fmt --file flag
-    var buffer = try allocator.alloc(u8, "--file=".len + file_path.len);
-    var writer = std.io.fixedBufferStream(buffer);
-    try std.fmt.format(writer.writer(), "--file={s}", .{file_path});
+    const expanded_file = try file_flag(allocator, file_path);
 
     // create full rust command
+    const full_command = try create_full_command(allocator, args, expanded_file);
+
+    try start_compiler(allocator, full_command);
+}
+
+const ParsingError = error{
+    InvalidFormat,
+};
+
+fn parse_args(allocator: std.mem.Allocator) ![][:0]u8 {
+    const cmd_args = try std.process.argsAlloc(allocator);
+    if (cmd_args.len < 2) {
+        std.debug.print("usage: {s} <file> <forward-args>", .{std.fs.path.basename(cmd_args[0])});
+        return ParsingError.InvalidFormat;
+    }
+    return cmd_args;
+}
+
+fn expand_file_path(allocator: std.mem.Allocator, path: []const u8) ![]const u8 {
+    var buf: [std.fs.MAX_PATH_BYTES]u8 = undefined;
+    const cwd = try std.os.getcwd(&buf);
+    const file_path = try std.fs.path.join(allocator, &[_][]const u8{ cwd, path });
+    std.debug.print("got path: {s}", .{file_path});
+    return file_path;
+}
+
+fn file_flag(allocator: std.mem.Allocator, path: []const u8) ![]const u8 {
+    var buffer = try allocator.alloc(u8, "--file=".len + path.len);
+    var writer = std.io.fixedBufferStream(buffer);
+    try std.fmt.format(writer.writer(), "--file={s}", .{path});
+    return buffer;
+}
+
+fn create_full_command(allocator: std.mem.Allocator, args: []const []const u8, expanded_file: []const u8) ![]const []const u8 {
     var full_command = try allocator.alloc([]const u8, command.len + args.len - 1);
     std.mem.copy([]const u8, full_command, &command);
-    full_command[command.len] = buffer;
+    full_command[command.len] = expanded_file;
     std.mem.copy([]const u8, full_command[(command.len + 1)..], args[2..]);
     std.debug.print("running: {s}", .{full_command});
+    return full_command;
+}
 
-    // setup rust backtrace
+fn start_compiler(allocator: std.mem.Allocator, full_command: []const []const u8) !void {
     var env_map = try std.process.getEnvMap(allocator);
     try env_map.put("RUST_BACKTRACE", "1");
 
