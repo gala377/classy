12e8fadc586a24f9f6bf20f41ee347c82721ce0f
gala377 <r.galczak@gmail.com>
2023-08-23 23:11:35 +0200

    make an alias fro str because I like it more

--- scripts/examples.zig
+++ scripts/examples.zig
@@ -1,8 +1,9 @@
 //usr/bin/env zig run "$0" -- "$@"; exit
 
 const std = @import("std");
+const str = []const u8;
 
-const command = [_][]const u8{ "cargo", "run", "-p=classyc", "--" };
+const command = [_]str{ "cargo", "run", "-p=classyc", "--" };
 
 pub fn main() !void {
     // setup allocator
@@ -20,7 +21,7 @@ const ParsingError = error{
     InvalidFormat,
 };
 
-fn parse_args(allocator: std.mem.Allocator) ![][:0]u8 {
+fn parse_args(allocator: std.mem.Allocator) ![]str {
     const cmd_args = try std.process.argsAlloc(allocator);
     if (cmd_args.len < 2) {
         std.debug.print("usage: {s} <file> <forward-args>", .{std.fs.path.basename(cmd_args[0])});
@@ -29,31 +30,31 @@ fn parse_args(allocator: std.mem.Allocator) ![][:0]u8 {
     return cmd_args;
 }
 
-fn expand_file_path(allocator: std.mem.Allocator, path: []const u8) ![]const u8 {
+fn expand_file_path(allocator: std.mem.Allocator, path: str) !str {
     var buf: [std.fs.MAX_PATH_BYTES]u8 = undefined;
     const cwd = try std.os.getcwd(&buf);
-    const file_path = try std.fs.path.join(allocator, &[_][]const u8{ cwd, path });
+    const file_path = try std.fs.path.join(allocator, &[_]str{ cwd, path });
     std.debug.print("got path: {s}", .{file_path});
     return file_path;
 }
 
-fn file_flag(allocator: std.mem.Allocator, path: []const u8) ![]const u8 {
+fn file_flag(allocator: std.mem.Allocator, path: str) !str {
     var buffer = try allocator.alloc(u8, "--file=".len + path.len);
     var writer = std.io.fixedBufferStream(buffer);
     try std.fmt.format(writer.writer(), "--file={s}", .{path});
     return buffer;
 }
 
-fn create_full_command(allocator: std.mem.Allocator, args: []const []const u8, expanded_file: []const u8) ![]const []const u8 {
-    var full_command = try allocator.alloc([]const u8, command.len + args.len - 1);
-    std.mem.copy([]const u8, full_command, &command);
+fn create_full_command(allocator: std.mem.Allocator, args: []const str, expanded_file: str) ![]const str {
+    var full_command = try allocator.alloc(str, command.len + args.len - 1);
+    std.mem.copy(str, full_command, &command);
     full_command[command.len] = expanded_file;
-    std.mem.copy([]const u8, full_command[(command.len + 1)..], args[2..]);
+    std.mem.copy(str, full_command[(command.len + 1)..], args[2..]);
     std.debug.print("running: {s}", .{full_command});
     return full_command;
 }
 
-fn start_compiler(allocator: std.mem.Allocator, full_command: []const []const u8) !void {
+fn start_compiler(allocator: std.mem.Allocator, full_command: []const str) !void {
     var env_map = try std.process.getEnvMap(allocator);
     try env_map.put("RUST_BACKTRACE", "1");
 
